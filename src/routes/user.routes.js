528dedd564e329a708533b4f728395b40afde442
juliobarbagallo <julio.barbagallo@gmail.com>
2023-04-21 16:00:17 -0300

    hotFeat

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,4 +1,8 @@
 import { Router } from "express";
 const router = Router()
+import * as userCtrl from "../controllers/user.controller";
+import { verifyToken, isAdmin } from "../middleware";
+
+router.post('/', [verifyToken, isAdmin], userCtrl.createUser)
 
 export default router
