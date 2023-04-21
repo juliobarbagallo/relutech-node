ea00db312a2e9a7efcfcfbb427c782ff640b1eec
juliobarbagallo <julio.barbagallo@gmail.com>
2023-04-21 15:51:05 -0300

    feat: Adds middleware for route protection

---
+++ src/middleware/authJwt.js
@@ -0,0 +1,36 @@
+import jwt from 'jsonwebtoken'
+import config from '../config'
+import User from '../models/User'
+import Role from "../models/Role";
+
+export const verifyToken = async (req, res, next) => {
+    try {
+        const token = req.headers["x-access-token"]
+
+        if(!token) return res.status(403).json({message: "No token provided."})
+
+        const decoded = jwt.verify(token, config.SECRET)
+        req.userId = decoded.id
+        
+        const user = await User.findById(req.userId, {password: 0})
+        console.log(user)
+        if(!user) return res.status(404).json({message: "no user found"})
+
+        next();
+    } catch (e) {
+        return res.status(500).json({message: 'Unauthorized'})
+    }
+}
+
+export const isAdmin = async (req, res, next) => {
+    const user = await User.findById(req.userId)
+    const roles =await Role.find({_id:{$in: user.roles}})
+
+    for(let i = 0; i < roles.length; i++) {
+        if (roles[i].name === "admin") {
+            next()
+            return;
+        }
+    }
+    return res.status(403).json({message: "Just for admins."})
+}
